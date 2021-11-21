2ef239e4e88a9ef2a210874a348132204a418338
bob-justbob <levan.qiqodze-nasidze14@geolab.edu.ge>
2021-11-21 18:07:06 +0400

    added homework

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
+// filter array sityvebis sigrdzis mixedvit edzebs romelia 6 ze meti da mere egeni gamoaq
+
+const arr1 = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const arr2 = ['doom', 'logg', 'wood', 'hello', 'nice', 'yeah'];
+const howlWord = arr1.concat(arr2)
+console.log(howlWord)
+// concat array aertianebs or arrays ertshi
